91d49dd3a3969891dafde6da6189bc9197c48dc1
paolobarrado <paolobarrado06@gmail.com>
2023-09-10 22:00:22 +0800

    initial commit

---
+++ MachineProblem.java
@@ -0,0 +1,15 @@
+public class MachineProblem
+{
+    public static void main(String[] args) {
+        System.out.println("University of the East \n" +
+                           "College of Engineering \n" +
+                           "Computer Engineering Department");
+
+        System.out.println("NCP2103: Object Oriented Programming \n" +
+                           "\tAsst.Prof. Errol John Antonio");
+
+        System.out.println("Today's weather is fine");
+        System.out.println("\"Tomorrow Begins in the East\"");
+        System.out.println("1/3 of 30 is 10");
+    }
+}
